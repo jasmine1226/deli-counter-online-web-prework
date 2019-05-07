0f8d342985308b5af5e7f1d17d53d81df2975b64
Jasmine Chen <jasmine1226@gmail.com>
2019-05-07 06:22:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,16 +6,17 @@ def line(line)
   if line.size == 0
     puts "The line is currently empty."
   else
-    puts.chomp "The line is currently:"
+    puts "The line is currently:"
     line.each_with_index do |name, index|
       num = index + 1
-      puts.chomp " #{num}. #{name}"
+      puts " #{num}. #{name}"
     end
   end
 end
 
 def take_a_number(line, name)
   line.push(name)
+  name = name.chomp
   num = line.length
   puts "Welcome, #{name}. You are number #{num} in line"
   return line
