e4f7de14a171ab971ab5a43eac60399eafb9ee03
Jasmine Chen <jasmine1226@gmail.com>
2019-05-07 05:46:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,15 @@
 
 katz_deli = []
 
-def line(array)
-  if array.size == 0
+def line(line)
+  if line.size == 0
     puts "The line is currently empty."
+  else
+    
+    puts "The line is currently:"
   end
+end
+
+def take_a_number(line, name)
+  line.push(name)
 end
